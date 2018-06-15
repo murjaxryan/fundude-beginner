ba1a22629fec7d7c26ba30f3e22af1b96bfaa2fb
Ryan Murphy <murjax@gmail.com>
2018-06-14 21:56:01 -0400

    Level 4

--- player.rb
+++ player.rb
@@ -1,17 +1,33 @@
 class Player
-  def play_turn(warrior)
-    if warrior_can_rest?(warrior)
-      warrior.rest!
-    else
-      proceed(warrior)
-    end
+  attr_reader :warrior
+  attr_accessor :health
+  def play_turn(received_warrior)
+    set_warrior(received_warrior)
+    play_action
+    update_health
   end
 
-  def warrior_can_rest?(warrior)
-    warrior.health < 20 && warrior.feel.empty?
+  def play_action
+    warrior_can_rest? ? warrior.rest! : proceed
   end
 
-  def proceed(warrior)
+  def warrior_can_rest?
+    not_losing_health? && warrior.health < 20 && warrior.feel.empty?
+  end
+
+  def not_losing_health?
+    !(health && warrior.health < health)
+  end
+
+  def proceed
     warrior.feel.empty? ? warrior.walk! : warrior.attack!
+  end
+
+  def update_health
+    self.health = warrior.health
+  end
+
+  def set_warrior(received_warrior)
+    @warrior = received_warrior
   end
 end
